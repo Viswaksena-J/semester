09e0dbb035df7f816749b9af63d8bd350b806678
Viswaksena Jayam <97895225+Viswaksena-J@users.noreply.github.com>
2023-10-30 15:14:59 +0530

    Maths lab-1 submission

--- 5thSem/maths/lab1.py
+++ 5thSem/maths/lab1.py
@@ -1,25 +1,63 @@
-m = 68.1#kg
+#Code by Viswaksena J/35
+
+mass = 68.1#kg
 cd = 0.25#lumped drag coefficient
 g = 9.81
 
+print("\033[3;32;40m" + "Example 1.1" + "\033[m")
+
 import numpy as np
-velocity = np.sqrt(g*m/cd) * np.tanh(np.sqrt(g*cd/m) * 12)
-print(velocity)
+import matplotlib.pyplot as plt
+
+t_values = []
+y_values = []
 
 for t in range(0, 16, 2):
-    print(t, np.sqrt(g*m/cd) * np.tanh(np.sqrt(g*cd/m) * t))
+    y = np.sqrt(g*mass/cd) * np.tanh(np.sqrt(g*cd/mass) * t)
+    t_values.append(t)
+    y_values.append(y)
+    print("t =", t, "y =", y)
 
+
+plt.plot(t_values, y_values, 'ro-')
+plt.xlabel('t,s')
+plt.ylabel('v,m/s')
+plt.title('Line Plot for Different t Values(Analytical Solution)')
+plt.grid(True)
+plt.legend()
+plt.show()
+
+print("------------------------------------")
+
+print("\033[3;32;40m" + "Example 1.2" + "\033[m")
 #Eulers method
 # V = U + at
 #v(ti+1)=v(ti)+ g−cdv(ti)2 (ti+1−ti) m
 # New value = old value + slope × step size
-import matplotlib.pyplot as plt
+
 velocity = 0
 t0 = 0
+
+t_values2 = []
+y_values2 = []
+
 for t in range(0, 16, 2):
-    velocity = velocity + (g-(cd/m)*(velocity**2)) * (t-t0)    
+    velocity = velocity + (g-(cd/mass)*(velocity**2)) * (t-t0)
+    t_values2.append(t)
+    y_values2.append(velocity)    
     print(t, velocity)
     velocity = velocity
     t0 = t
-plt.plot(t, velocity, 'ro')
+
+plt.figure(figsize=(8, 6))
+plt.plot(t_values, y_values, 'ro-', label='Analytical Solution')
+plt.plot(t_values2, y_values2, 'bo-', label='Numerical Solution')
+plt.xlabel('t,s')
+plt.ylabel('v,m/s')
+plt.title('Comparison of the numerical and analytical solutions for the bungee jumper problem')
+plt.grid(True)
+plt.legend()
+
+plt.show()
+
     
